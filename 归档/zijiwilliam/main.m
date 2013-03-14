7d0fec2bfe3ff8e7a0a801c385b391cc0163a5fa
tcwilliam <wangwl90@gmail.com>
2013-03-14 11:02:09 -0500

    will

---
+++ 归档/zijiwilliam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  zijiwilliam
+//
+//  Created by 文力 王 on 13-3-10.
+//  Copyright (c) 2013年 文力 王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
